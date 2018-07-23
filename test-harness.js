602f8718a8fd703080df0ecb4b970065c1625710
Joey Ciechanowicz <joeyciechanowicz@users.noreply.github.com>
2018-07-23 09:30:22 +0100

    added test harness

---
+++ test-harness.js
@@ -0,0 +1,34 @@
+const {slackToken} = require('./config.json');
+const {meeting} = require('.');
+
+const requestStub = {
+    method: 'POST',
+    body: {
+        token: '',
+        team_id: '',
+        team_domain: '',
+        channel_id: '',
+        channel_name: 'meeting-test',
+        user_id: '',
+        user_name: '',
+        command: '/meeting',
+        text: 'start',
+        response_url: '',
+        trigger_id: ''
+    }
+};
+
+const responseStub = {
+    json(obj) {
+        console.log(obj);
+    }
+};
+
+meeting(requestStub, responseStub)
+    .then(x => {
+        process.exit();
+    })
+    .catch(e => {
+        console.error(e);
+        process.exit();
+    });
